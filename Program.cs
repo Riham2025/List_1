6369076fc45bd3521e60883487e14042ca2a1f24
riham <riham91.alsiyabi@gmail.com>
2025-04-16 10:40:16 +0400

    // Top N Frequent Numbers>> startr

--- Program.cs
+++ Program.cs
@@ -5,6 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            // Top N Frequent Numbers>Given a List<int> of numbers, find the top N most frequent numbers
+
+            Console.WriteLine("Enter the numbers :");
+            Console.WriteLine("Enter the number of top frequent numbers you want to find :");
+
+
         }
     }
 }
