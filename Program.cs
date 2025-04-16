690703aefd44d79b02edb73f73829f721d9e42b9
riham <riham91.alsiyabi@gmail.com>
2025-04-16 11:47:04 +0400

    // Top N Frequent Numbers>Given a List<int> of numbers, find the top N most frequent numbers

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace List_1
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace List_1
 {
     internal class Program
     {
@@ -7,10 +9,33 @@
             Console.WriteLine("Hello, World!");
             // Top N Frequent Numbers>Given a List<int> of numbers, find the top N most frequent numbers
 
-            Console.WriteLine("Enter the numbers :");
-            Console.WriteLine("Enter the number of top frequent numbers you want to find :");
+            {
+                List<int> numbers = new List<int> { 50, 10, 50, 12, 10, 30, 50, 20, 12 };
+                int N = 2;
 
+                List<int> result = GetTopNFrequentNumbers(numbers, N);
 
+                Console.WriteLine("Top " + N + " frequent numbers:");
+                foreach (int num in result)
+                {
+                    Console.WriteLine(num);
+                }
+            }
+
+            static List<int> GetTopNFrequentNumbers(List<int> numbers, int n)
+            {
+                return numbers
+                    .GroupBy(num => num)                          // We collect the similar numbers
+                    .OrderByDescending(group => group.Count())    // We arrange them according to the number of repetitions.
+                    .Take(n)                                      // We take the first N numbers.
+                    .Select(group => group.Key)                   // Return the numbers only.
+                    .ToList();
+            }
         }
+
+
+
+
     }
+    
 }
